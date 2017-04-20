f1e9b8d102e1744f331f850cba03496956846181
AJ Webb <webbushka@gmail.com>
2017-04-19 23:24:09 -0600

    Fixes Closure proptype, adds externals to reduce index.js size, and adds prop-types package

--- webpack.config.js
+++ webpack.config.js
@@ -14,5 +14,25 @@ module.exports = [{
         loader: "babel?presets[]=es2015,presets[]=react,presets[]=stage-0,plugins[]=transform-object-rest-spread"
       }
     ]
+  },
+  externals: {
+    lodash: {
+      amd: "lodash",
+      commonjs: "lodash",
+      commonjs2: "lodash",
+      root: "_"
+    },
+    "prop-types": {
+      amd: "prop-types",
+      commonjs2: "prop-types",
+      commonjs: "prop-types",
+      root: "PropTypes"
+    },
+    react: {
+      amd: "react",
+      commonjs2: "react",
+      commonjs: "react",
+      root: "React"
+    }
   }
 }];
